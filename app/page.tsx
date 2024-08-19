74479518df0b24bec6cac67093eda14ac0b6db9d
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-08-19 20:56:51 +0200

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1>Home
+      <Button variant="outline">Bucket</Button>
+    </h1>
+    
+  );
+}
